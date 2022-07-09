216532490bad37b746a9ed8430f1c627c9b14da2
aarbys <104789796+aarbys@users.noreply.github.com>
2022-07-09 10:59:32 +0300

    Add files via upload

--- EGE_trainer.py
+++ EGE_trainer.py
@@ -1,14 +1,14 @@
 import third  # есть
 import four
 import five
-import six
+import seven
 import nine  # есть
 import fourteen  # есть
 import sixteen  # есть
 import seventeen  # есть
 import eighteen  # есть
 import games  # есть
-import twenty_two  # есть
+import twenty_two_six  # есть
 import twenty_three  # есть
 import twenty_four  # есть
 import twenty_five  # есть
@@ -17,11 +17,13 @@ import twenty_seven  # есть
 
 
 # Need to create new 26 and other
-
-
+# Цифры и дефис не использовать в папках
+# Классы с большой
+# Маленькая и нижние подчеркивания в остальном
 def check_answer(correct_answer, number):
     two_answers = [27, 26, 18, 17]
     more_two = [19, 20, 21, 25]
+    print('Вводите ответ:')
     if number in two_answers:
         answer = input()
         answer = answer.split()
@@ -34,6 +36,7 @@ def check_answer(correct_answer, number):
         elif correct_answer[0] == answer[0] or correct_answer[1] == answer[1] or (
                 correct_answer[0] == answer[1] and correct_answer[1] == answer[0]):
             print('Почти получилось. 1 балл.')
+            print(*correct_answer)
         else:
             print('Попробуйте в другой раз. 0 баллов')
             print('Хотите узнать правильный ответ? (Да/Нет)')
@@ -46,7 +49,7 @@ def check_answer(correct_answer, number):
         exit()
     elif number in more_two:
         print('Вводите ответы построчно.')
-        if number == 25 and len(correct_answer[0])>1:
+        if number == 25 and len(correct_answer[0]) > 1:
             print('Вводите ответ:')
             for i in range(len(correct_answer)):
                 x = input().split()
@@ -115,6 +118,7 @@ def check_answer(correct_answer, number):
                 print(correct_answer)
         exit()
 
+
 def start():
     print('Выберите номер задания')
     x = int(input())
@@ -129,7 +133,7 @@ def start():
     elif x == 5:
         c_a = five.question()
     elif x == 6:
-        c_a = six_1.question()
+        c_a = twenty_two_six.question_6()
     elif x == 7:
         c_a = seven.question()
     elif x == 8:
@@ -157,7 +161,7 @@ def start():
     elif x == 19 or x == 20 or x == 21:
         c_a = games.question()  # есть
     elif x == 22:
-        c_a = twenty_two.question()  # есть
+        c_a = twenty_two_six.question_22()  # есть
     elif x == 23:
         c_a = twenty_three.question()  # есть
     elif x == 24:
---
+++ twenty_two_six.py
@@ -0,0 +1,193 @@
+import sqlite3 as sl
+import random
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+def question_6():
+    amount_of_id = None
+    con = sl.connect('six.db')
+    d = con.execute("SELECT COUNT(id) from PROBLEMS")
+    for c in d:
+        amount_of_id = int(*c)
+    x = random.randint(89, 89)
+    print('Ниже на нескольких языках будет написан алгоритм.')
+    with con:
+        output = con.execute("SELECT problem_txt from PROBLEMS WHERE id == {}".format(x))
+        answer = con.execute("SELECT answer from PROBLEMS WHERE id == {}".format(x))
+        for c in answer:
+            correct_answer = int(*c)
+        for c in output:
+            print(*c)
+
+        output_code(x, 6)
+    return correct_answer
+
+
+def question_22():
+    amount_of_id = None
+    con = sl.connect('twenty_two.db')
+    d = con.execute("SELECT COUNT(id) from PROBLEMS")
+    for c in d:
+        amount_of_id = int(*c)
+    x = random.randint(1, amount_of_id)  # 1,107
+    print('Ниже на нескольких языках будет написан алгоритм.')
+    with con:
+        output = con.execute("SELECT problem_txt from PROBLEMS WHERE id == {}".format(x))
+        answer = con.execute("SELECT answer from PROBLEMS WHERE id == {}".format(x))
+        for c in answer:
+            correct_answer = int(*c)
+        for c in output:
+            print(*c)
+
+    output_code(x, 22)
+    return correct_answer
+
+
+def output_code(x, connection_to_BD):
+
+    def create_4_position(ime):
+
+        def create_lines(max_wid, k_w, max_hei, k_h):
+            img1 = ImageDraw.Draw(ime)
+            img1.line([(max_wid - k_w, 0), (max_wid - k_w, image_height)], fill=(0, 0, 0), width=10)
+            img1.line([(0, max_hei - k_h), (image_width, max_hei - k_h)], fill=(0, 0, 0), width=10)
+
+        width, height = 20, max(text_to_draw_image[0].height, text_to_draw_image[1].height)
+
+        m_h = max(image_to_draw[0].height, image_to_draw[1].height)
+
+        m_w = max(image_to_draw[0].width, image_to_draw[3].width)
+
+        m_h_in_3_4_txt = max(text_to_draw_image[3].height, text_to_draw_image[4].height)
+
+        ime.paste(text_to_draw_image[0], (20, 10))
+        ime.paste(text_to_draw_image[1], (m_w + 50, 10))
+        ime.paste(text_to_draw_image[3], (20, m_h + 55 + height))
+        ime.paste(text_to_draw_image[4], (m_w + 50, m_h + 55 + height))
+
+        create_lines(0, 0, height, 25)
+        create_lines(0, 0, height + m_h_in_3_4_txt + m_h, -15)
+
+        m_w += 65
+        m_h += 55
+
+        create_lines(m_w, 35, m_h, 45 - height)
+
+        m_h += m_h_in_3_4_txt + height
+        ime = paste_image(ime, image_to_draw[0], 20, height)
+        ime = paste_image(ime, image_to_draw[1], m_w+20, height)
+        ime = paste_image(ime, image_to_draw[3], 20, m_h)
+        ime = paste_image(ime, image_to_draw[4], m_w+20, m_h)
+
+    prob_lang = ['c', 'python', 'basic', 'algorithm', 'pascal']
+
+    def finding_resolution_and_font_type(txt):
+        image = Image.new('RGB', (200, 100), (255, 255, 255))
+        dr = ImageDraw.Draw(image)
+        f_t = ImageFont.truetype("arial.ttf", 65)
+        t_wi, t_he = dr.textsize(txt, font=f_t)
+        return t_wi, t_he, f_t
+
+    def taking_problem():
+        if connection_to_BD == 22:
+            con = sl.connect('twenty_two.db')
+        else:
+            con = sl.connect('six.db')
+        strings_out = []
+        for j in prob_lang:
+            with con:
+                output = con.execute("SELECT {} from PROBLEMS WHERE id == {}".format(j, x))
+                for v in output:
+                    b = str(*v).replace(' ', ' ')
+                    strings_out.append(b)
+        return strings_out
+
+    def paste_image(main_image: Image, image_to_paste, coordinate_x, coordinate_y):
+        main_image.paste(image_to_paste, (coordinate_x, coordinate_y))
+        return main_image
+
+    strings = taking_problem()
+    image_to_draw = []
+    flag_for_none = False
+    for k in range(len(strings)):
+        text = strings[k]
+        text_width, text_height, font_type = finding_resolution_and_font_type(text)
+
+        im = Image.new('RGB', (text_width + 45, text_height + 45), (255, 255, 255))
+
+        draw = ImageDraw.Draw(im)
+
+        d = draw
+
+        d.text(xy=(2, 2), text=text, fill=(0, 0, 0), font=font_type)
+
+        if text == 'None':
+            flag_for_none = True
+        image_to_draw.append(im)
+
+    resol = []
+    max_height_1, max_height_2 = 0, 0
+    max_width_1, max_width_2 = 0, 0
+    for i in image_to_draw:
+        resol.append([i.width, i.height])
+        if len(image_to_draw) == 5 and image_to_draw[2] == i:
+            continue
+        if max_height_1 < i.height:
+            max_height_2 = max_height_1
+            max_height_1 = i.height
+        else:
+            max_height_2 = max(max_height_2, i.height)
+        if max_width_1 < i.width:
+            max_width_2 = max_width_1
+            max_width_1 = i.width
+        else:
+            max_width_2 = max(max_width_2, i.width)
+
+    image_height = max_height_1 + max_height_2 + 300
+    image_width = max_width_1 + max_width_2 + 300
+    img = Image.new('RGB', (image_width, image_height), (255, 255, 255))
+
+    text_to_draw_image = []
+
+    prob_lang = ['c++', 'python', 'basic', 'algorithm', 'pascal']
+
+    for k in prob_lang:
+        t_w, t_h, font_type = finding_resolution_and_font_type(k)
+        im = Image.new('RGB', (t_w + 45, t_h + 45), (255, 255, 255))
+
+        draw = ImageDraw.Draw(im)
+
+        d = draw
+
+        d.text(xy=(2, t_h - 2), text=k, fill=(0, 0, 0), font=font_type, anchor='lb')
+        text_to_draw_image.append(im)
+
+    if flag_for_none:
+        create_4_position(img)
+    else:
+        create_4_position(img)
+        max_h = max(text_to_draw_image[0].height, text_to_draw_image[1].height)
+
+        image_width_last = image_width + image_to_draw[2].width + 75
+        last_img = Image.new('RGB', (image_width_last, image_height), (255, 255, 255))
+        img_for_line = ImageDraw.Draw(last_img)
+
+        img_for_line.line([(image_width, 0), (image_width, image_height)], fill=(0, 0, 0), width=10)
+
+        last_img.paste(img, (0, 0))
+
+        last_img.paste(image_to_draw[2], (image_width + 45, text_to_draw_image[1].height))
+
+        last_img.paste(text_to_draw_image[2], (image_width + 50, 10))
+
+        img_for_line.line([(0, max_h - 25), (image_width_last, max_h - 25)], fill=(0, 0, 0), width=10)
+
+        img = last_img.copy()
+
+    img.show()
+
+
+if __name__ == '__main__':
+    question_6()
